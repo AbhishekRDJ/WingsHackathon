bb95d3dbef0bedd06b001c7580ab54617f6aea9f
aditya <magaraditya2911@gmail.com>
2025-02-02 21:10:25 +0530

    main screen and project structure added

---
+++ sign_bridge/lib/mainscreen/mainscreen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:sign_bridge/pages/login_page.dart';
+import 'package:sign_bridge/pages/signup_page.dart';
+import 'package:sign_bridge/utils/colors.dart';
+import 'package:sign_bridge/widgets/custome_button.dart';
+
+class Mainscreen extends StatefulWidget {
+  const Mainscreen({super.key});
+
+  @override
+  State<Mainscreen> createState() => _MainscreenState();
+}
+
+class _MainscreenState extends State<Mainscreen> {
+  bool _animate = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        _animate = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: AnimatedContainer(
+              duration: Duration(seconds: 2),
+              curve: Curves.easeOut,
+              transform: Matrix4.translationValues(0,
+                  _animate ? -MediaQuery.of(context).size.height * 0.19 : 0, 0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    curve: Curves.easeOut,
+                    height: _animate ? 250 : 250, // Logo reaches max size first
+                    child: Image.asset("assets/images/logo.png"),
+                  ),
+                  const SizedBox(height: 30),
+                  AnimatedOpacity(
+                    duration: Duration(seconds: 2),
+                    opacity: _animate ? 1 : 0,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Text(
+                            "Connecting through",
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Text(
+                            "Signs!",
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: MediaQuery.of(context).size.height * 0.18,
+            left: 0,
+            right: 0,
+            child: AnimatedOpacity(
+              duration: Duration(seconds: 2),
+              opacity: _animate ? 1 : 0,
+              child: Column(
+                children: [
+                  CustomeButton(
+                      bgColor: Colors.white,
+                      text: "Sign Up",
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const SignupPage()));
+                      }),
+                  const SizedBox(height: 20),
+                  CustomeButton(
+                    bgColor: bgColor,
+                    text: "Log In",
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const LoginPage()));
+                    },
+                    isBorder: true,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
