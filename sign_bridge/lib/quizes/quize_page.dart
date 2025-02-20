a01c0ce92f80fa7678f6addeb7cf4e70039b73ad
aditya <magaraditya2911@gmail.com>
2025-02-20 21:43:58 +0530

    ui changes

--- sign_bridge/lib/quizes/quize_page.dart
+++ sign_bridge/lib/quizes/quize_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:sign_bridge/quizes/quiz.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:sign_bridge/utils/card_building.dart';
 import 'package:sign_bridge/utils/data.dart';
 
 class HomePageOfQuize extends StatelessWidget {
@@ -27,7 +28,7 @@ class HomePageOfQuize extends StatelessWidget {
                         size: 32,
                       )),
                   Text(
-                    "User Name",
+                    "Quizzes and Games",
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 28,
@@ -94,7 +95,7 @@ class HomePageOfQuize extends StatelessWidget {
                       },
                     ];
                     final item = items[index];
-                    return _buildCard(
+                    return CardBuilder.buildCard(
                             item["title"] as String,
                             item["startColor"] as Color,
                             item["endColor"] as Color,
@@ -103,10 +104,11 @@ class HomePageOfQuize extends StatelessWidget {
                             item['onTap'] as VoidCallback,
                             item['url'] as String)
                         .animate()
-                        .moveX(
-                          begin: index % 2 == 0 ? -200 : 200,
-                          end: 0,
+                        .scale(
+                          begin: Offset.zero,
+                          end: Offset(1, 1),
                           duration: 600.ms,
+                          delay: (index * 200).ms,
                         );
                   },
                 ),
@@ -115,62 +117,6 @@ class HomePageOfQuize extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-
-  Widget _buildCard(String title, Color startColor, Color endColor,
-      BuildContext context, double height, VoidCallback onTap, String imgUrl) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Stack(children: [
-        AnimatedContainer(
-          duration: const Duration(milliseconds: 300),
-          height: height, // Dynamic height per card
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [startColor, endColor],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.circular(16),
-            boxShadow: [
-              BoxShadow(
-                color: endColor.withOpacity(0.5),
-                blurRadius: 8,
-                offset: const Offset(4, 4),
-              ),
-            ],
-          ),
-          child: Opacity(
-            opacity: 1,
-            child: imgUrl == ''
-                ? SizedBox(
-                    width: height,
-                  )
-                : ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Image.network(
-                      imgUrl,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-          ),
-        ),
-        Positioned(
-          top: 10,
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              title,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: Colors.white)
-                  .copyWith(color: Colors.black),
-            ),
-          ),
-        ),
-      ]),
     );
   }
 }
--- sign_bridge/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sign_bridge/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,12 @@ import Foundation
 
 import flutter_tts
 import path_provider_foundation
-import speech_to_text_macos
+import speech_to_text
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
